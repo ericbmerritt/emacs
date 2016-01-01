2cb9d7d2f3860b9f573591b0dcda8c588561a9ff
Eric Merritt <eric@merritt.tech>
2016-01-01 10:44:59 -0800

    fix spelling in nix environments

---
+++ emacsrc/start-files/spelling.el
@@ -0,0 +1,7 @@
+(cond
+ ((executable-find "aspell")
+  (setq ispell-program-name "aspell")
+  (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US")))
+ ((executable-find "hunspell")
+  (setq ispell-program-name "hunspell")
+  (setq ispell-extra-args '("-d en_US"))))
